6d3c045c085fd959d32c0983a0f59c143362807d
zohaib ahmed <zohaib.ahmad@northbaysolutions.net>
2019-02-13 17:05:00 +0500

    remove s3locationparam and replace with location

--- tpcds-setup-nbs.sh
+++ tpcds-setup-nbs.sh
@@ -105,7 +105,7 @@ do
 	    --hivevar DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} --hivevar SOURCE=tpcds_text_${SCALE} \
             --hivevar SCALE=${SCALE} \
 	    --hivevar REDUCERS=${REDUCERS} \
-	    --hivevar FILE=${FORMAT} --hivevar S3LOCATION=${LOCATION}"
+	    --hivevar FILE=${FORMAT} --hivevar LOCATION=${LOCATION}"
 	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
 	i=`expr $i + 1`
 done
@@ -117,7 +117,7 @@ do
             --hivevar SCALE=${SCALE} \
 	    --hivevar SOURCE=tpcds_text_${SCALE} --hivevar BUCKETS=${BUCKETS} \
 	    --hivevar RETURN_BUCKETS=${RETURN_BUCKETS} --hivevar REDUCERS=${REDUCERS} --hivevar FILE=${FORMAT} \
-			--hivevar S3LOCATION=${LOCATION} "
+			--hivevar LOCATION=${LOCATION} "
 	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
 	i=`expr $i + 1`
 done
